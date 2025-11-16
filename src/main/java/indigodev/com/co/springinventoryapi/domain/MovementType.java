b6eedea36a90acc3481b96f5b342ff2b1e4910df
MiguelDev <migopa0222@gmail.com>
2025-11-16 01:45:10 -0500

    Module 1: Task 5 creates an ENUM to definitive the types o movements

---
+++ src/main/java/indigodev/com/co/springinventoryapi/domain/MovementType.java
@@ -0,0 +1,14 @@
+package indigodev.com.co.springinventoryapi.domain;
+
+
+public enum MovementType {
+    /**
+     * Represents a movement where stock is added to the inventory.
+     */
+    ENTRY,
+
+    /**
+     * Represents a movement where stock is removed from the inventory.
+     */
+    EXIT
+}
