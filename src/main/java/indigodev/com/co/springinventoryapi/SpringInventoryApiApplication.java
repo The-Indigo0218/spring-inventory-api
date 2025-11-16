c3e96b1e486a4202ec141fc0e7e9b12ac5b21e1e
MiguelDev <migopa0222@gmail.com>
2025-11-15 23:29:24 -0500

    Initial configurations

---
+++ src/main/java/indigodev/com/co/springinventoryapi/SpringInventoryApiApplication.java
@@ -0,0 +1,13 @@
+package indigodev.com.co.springinventoryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInventoryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInventoryApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/indigodev/com/co/springinventoryapi/SpringInventoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package indigodev.com.co.springinventoryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInventoryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
