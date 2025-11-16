86d76ea358a91e6a13a136d87d68d7a923d7fd71
MiguelDev <migopa0222@gmail.com>
2025-11-15 23:49:24 -0500

    Module 1: Task 1 add @EnableJpaAuditing at main

--- src/main/java/indigodev/com/co/springinventoryapi/SpringInventoryApiApplication.java
+++ src/main/java/indigodev/com/co/springinventoryapi/SpringInventoryApiApplication.java
@@ -2,8 +2,10 @@ package indigodev.com.co.springinventoryapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringInventoryApiApplication {
 
     public static void main(String[] args) {
