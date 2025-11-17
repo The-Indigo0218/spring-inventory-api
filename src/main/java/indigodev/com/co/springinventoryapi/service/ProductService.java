1dbf2529bee72d5b4f0cf065f460cbe27cec2aea
MiguelDev <migopa0222@gmail.com>
2025-11-16 23:48:25 -0500

    FT: save product now recibe CreateProductRequest dto at param

--- src/main/java/indigodev/com/co/springinventoryapi/service/ProductService.java
+++ src/main/java/indigodev/com/co/springinventoryapi/service/ProductService.java
@@ -1,9 +1,10 @@
 package indigodev.com.co.springinventoryapi.service;
 
 import indigodev.com.co.springinventoryapi.domain.Product;
+import indigodev.com.co.springinventoryapi.dto.product.CreateProductRequest;
 
 public interface ProductService {
-    Product save(String name);
+    Product save(CreateProductRequest createProductRequest);
     Product fiendById(int id);
     Product fiendByName(String name);
     void delete(int id);
