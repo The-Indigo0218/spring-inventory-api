c3ad31a5300c1a12ff73ca1e4575979b8a5c23ff
MiguelDev <migopa0222@gmail.com>
2025-11-19 14:43:35 -0500

    FT: Task 7 module 2: create a ProductController (get and post)

---
+++ src/main/java/indigodev/com/co/springinventoryapi/controller/ProductController.java
@@ -0,0 +1,30 @@
+package indigodev.com.co.springinventoryapi.controller;
+
+import indigodev.com.co.springinventoryapi.dto.request.product.CreateProductRequest;
+import indigodev.com.co.springinventoryapi.dto.response.product.ProductResponse;
+import indigodev.com.co.springinventoryapi.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/products")
+public class ProductController {
+    private final ProductService productService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getProductById(@PathVariable Long id){
+        ProductResponse response = productService.findById(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> createProduct(@RequestBody CreateProductRequest request){
+        ProductResponse response = productService.createProduct(request);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+    }
+}
