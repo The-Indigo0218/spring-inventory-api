98362995356b01098e31ffda85306805f9aaf9c3
MiguelDev <migopa0222@gmail.com>
2025-11-19 11:38:25 -0500

    Util mapperClass

---
+++ src/main/java/indigodev/com/co/springinventoryapi/util/InventoryMapper.java
@@ -0,0 +1,22 @@
+package indigodev.com.co.springinventoryapi.util;
+
+import indigodev.com.co.springinventoryapi.domain.Movement;
+import indigodev.com.co.springinventoryapi.domain.Product;
+import indigodev.com.co.springinventoryapi.dto.response.movement.MovementResponse;
+import indigodev.com.co.springinventoryapi.dto.response.product.ProductResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InventoryMapper {
+
+    private ProductResponse mapToResponseProduct(Product product) {
+        return new ProductResponse(product.getId(), product.getName(), product.getStock(), product.getCreatedAt());
+    }
+
+    private MovementResponse mapToResponseMovement(Movement movement) {
+        ProductResponse productResponse = mapToResponseProduct(movement.getProduct());
+        return new MovementResponse(productResponse, movement.getType().toString(), movement.getQuantity(), movement.getCreatedAt());
+    }
+
+
+}
