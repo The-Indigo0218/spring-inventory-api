4ff458467def8cb86e162dc6064d8e42a458daf7
MiguelDev <migopa0222@gmail.com>
2025-11-19 19:38:50 -0500

    FT: In MovementController find product by name

--- src/main/java/indigodev/com/co/springinventoryapi/controller/MovementController.java
+++ src/main/java/indigodev/com/co/springinventoryapi/controller/MovementController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/movements")
@@ -27,6 +29,11 @@ public class MovementController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(response);
+    }
+
+    @GetMapping("/product/{name}")
+    public ResponseEntity<List<MovementResponse>> findByProductName(@PathVariable String name) {
+        return ResponseEntity.ok(movementService.findByProductName(name));
     }
 
 }
