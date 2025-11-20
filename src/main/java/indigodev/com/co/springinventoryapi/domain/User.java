5f1eeee3d5fa0fa414ce8077569cbe2505230a06
MiguelDev <migopa0222@gmail.com>
2025-11-20 15:09:57 -0500

    FT: User class to define the roles and authorities

---
+++ src/main/java/indigodev/com/co/springinventoryapi/domain/User.java
@@ -0,0 +1,64 @@
+package indigodev.com.co.springinventoryapi.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import indigodev.com.co.springinventoryapi.domain.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true, onlyExplicitlyIncluded = true)
+@SuperBuilder(toBuilder = true)
+@Table(name = "users")
+public class User extends BaseEntity implements UserDetails {
+
+    @Column( nullable = false ,unique = true)
+    private String username;
+
+    @JsonIgnore
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
