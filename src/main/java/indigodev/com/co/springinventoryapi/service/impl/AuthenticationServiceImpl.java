dfb71231d198bd4e783e69de8a09705cde0585ba
MiguelDev <migopa0222@gmail.com>
2025-11-24 16:39:13 -0500

    FX (AUTH): Add validateRoleHierarchy at register to validate the permission

--- src/main/java/indigodev/com/co/springinventoryapi/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/indigodev/com/co/springinventoryapi/service/impl/AuthenticationServiceImpl.java
@@ -15,6 +15,7 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -44,6 +45,9 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public AuthenticationResponse register(RegisterRequest request) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        validateRoleHierarchy(authentication, request.role());
+
         UserRole role = enumMapper.userRoleMapper(request.role());
         User user = User.builder()
                 .username(request.username())
