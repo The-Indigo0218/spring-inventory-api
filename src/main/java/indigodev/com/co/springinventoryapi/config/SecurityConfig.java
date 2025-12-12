f3a14ae8b71a4fa6b84b099166dd6afda24221f4
MiguelDev <migopa0222@gmail.com>
2025-12-12 01:07:20 -0500

    chore: final release preparation  -Fixed SecurityConfig path matcher syntax - Finalized project structure for MVP release

--- src/main/java/indigodev/com/co/springinventoryapi/config/SecurityConfig.java
+++ src/main/java/indigodev/com/co/springinventoryapi/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("api/v1/auth/login").permitAll()
+                        .requestMatchers("/api/v1/auth/login").permitAll()
                         .requestMatchers(
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
